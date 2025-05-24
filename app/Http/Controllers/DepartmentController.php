a4cee6b5750fe936860200cb06ad305fd7f00247
JHenrique <henryque.pc.game@gmail.com>
2025-05-24 09:50:08 -0300

    GGestão de Departamentos - Parte 2

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DepartmentController extends Controller
 {
     public function index()
     {
+        Auth::user()->can('admin') ? : abort(403, 'You are not allowed to access this page');
+
         $departments = Department::all();
         return view('department.departments', compact('departments'));
     }
