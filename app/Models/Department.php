3a1e9d0e556887981b9a29f7073a788847fa871d
JHenrique <henryque.pc.game@gmail.com>
2025-05-24 10:35:27 -0300

    Adicionar Departamento

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Department extends Model
 {
     //
+    protected $fillable = ['name'];
     public function users()
     {
         // each department can be filled with multiple user
