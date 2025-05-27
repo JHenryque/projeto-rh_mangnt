7501608a22802a629f8bfb46cc0552d851d62b1a
JHenrique <henryque.pc.game@gmail.com>
2025-05-27 12:39:02 -0300

    Adicionar User Details - Parte 2

--- app/Models/UserDetail.php
+++ app/Models/UserDetail.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserDetail extends Model
 {
-    //
+    protected $fillable = [
+        'address',
+        'zip_code',
+        'city',
+        'phone',
+        'salary',
+        'admission_date',
+    ];
     public function user()
     {
         // each has one user_details / each user_details belongs to a single user
