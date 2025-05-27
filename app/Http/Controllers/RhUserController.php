dd250a92c657eee493c482611a09825a7fbdfeb5
JHenrique <henryque.pc.game@gmail.com>
2025-05-27 10:57:28 -0300

    Listar Departamentos Disponíveis

--- app/Http/Controllers/RhUserController.php
+++ app/Http/Controllers/RhUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,6 +22,8 @@ class RhUserController extends Controller
     {
         Auth::user()->can('admin') ? : abort(403, 'You are not allowed to access this page');
 
-        return view('colaborators.add-rh-user');
+        $departments = Department::all();
+
+        return view('colaborators.add-rh-user', compact('departments'));
     }
 }
--- resources/views/colaborators/add-rh-user.blade.php
+++ resources/views/colaborators/add-rh-user.blade.php
@@ -30,7 +30,18 @@
                             @enderror
                         </div>
 
-                        <p class="mb-3">Profile: <strong>Human Resources</strong></p>
+                         <div class="mb-3">
+                             <label for="select_department" class="form-label">Department</label>
+                             <select class="form-select" name="department" id="department">
+                                 @foreach($departments as $department)
+
+                                    <option value="{{$department->id}}">{{ $department->name }}</option>
+
+                                 @endforeach
+                             </select>
+                         </div>
+
+                        <p class="mb-3">Profile: <strong></strong></p>
 
                         <div class="mb-3">
                             <a href="{{ route('colaborators.rh-users') }}" class="btn btn-outline-danger me-3">Cancel</a>
