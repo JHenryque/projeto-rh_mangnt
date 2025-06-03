44591cf9355e999eaa12ee61adbc1365a3d004f5
JHenrique <henryque.pc.game@gmail.com>
2025-06-03 11:28:15 -0300

    Página Resumo Para o Administrador - parte 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -16,6 +16,40 @@ class AdminController extends Controller
     {
         Auth::user()->can('admin') ? : abort(403, 'You are not allowed to access this page');
 
+        // colleact all infomation about the organization
+        $data = [];
+
+        // get total number of colaboratos (deleted_at is null)
+        $data['total_colaborators'] = User::whereNull('deleted_at')->count();
+
+        // total colaborators deleted
+        $data['total_colaborators_deleted'] = User::onlyTrashed()->count();
+
+        // total salary for all colaboratos
+        $data['total_salary'] = User::withoutTrashed()->with('detail')->get()->sum(function ($colaborator) {
+            return $colaborator->detail->salary;
+        });
+
+        // total colaborators by department
+        $data['total_colaborators_per_department'] = User::withoutTrashed()->with('department')->get()->groupBy('department_id')->map(function ($department) {
+            return [
+                'department' => $department->first()->department->name ?? " - ",
+                'total' => $department->count(),
+            ];
+        });
+
+        // total salaru by department
+        $data['total_colaborators_by_department'] = User::withoutTrashed()->with('department', 'detail')->get()->groupBy('department_id')->map(function ($department) {
+            return [
+                'department' => $department->first()->department->name ?? " - ",
+                'total' => $department->sum(function ($colaborator) {
+                    return $colaborator->detail->salary;
+                }),
+            ];
+        });
+
+        dd($data);
+
         return view('home');
     }
 
